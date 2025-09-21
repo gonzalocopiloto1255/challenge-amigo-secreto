033b60277a124b9e88c712dca2507109e0644e79
Gonzalo Gil <gonzalogil1255@gmail.com>
2025-09-20 21:36:50 -0500

    feat: Desafío 1 - Crear array para almacenar nombres

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Desafío 1: Crear un array para almacenar los nombres
+let amigos = [];
